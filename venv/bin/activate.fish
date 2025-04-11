d3e081425885ae371cd9eeafa2bfc89770198d0a
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-04-10 20:07:09 -0700

    Updated README

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/malhar/Weather-App/venv"
+set -gx VIRTUAL_ENV "/Users/malhar/ETL_weather/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
